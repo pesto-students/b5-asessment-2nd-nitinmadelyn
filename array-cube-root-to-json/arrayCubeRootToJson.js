efb7b86d465abe4961a44d7d082784afffb16d00
nitinmadelyn <nitin@Nitins-MacBook-Pro.local>
2020-12-21 14:47:59 +0530

    assignment done

--- array-cube-root-to-json/arrayCubeRootToJson.js
+++ array-cube-root-to-json/arrayCubeRootToJson.js
@@ -1,3 +1,61 @@
-const arrayCubeRootToJson = arr => {};
+const invalidInput = [true, false, null, undefined];
+const cubes = [1, 8, 27, 64, 125, 216, 343, 512, 729, 1000];
 
-export { arrayCubeRootToJson };
+const arrayCubeRootToJson = arr => {
+    if(!Array.isArray(arr) || arr.length == 0){
+        return "Invalid input/not an array.";
+    }
+    var commonValues = arr.filter(function(n) {
+        if(invalidInput.indexOf(n) !== -1) {
+            return invalidInput.indexOf(n);
+        } else if(typeof(n) === "string"){
+            if(parseInt(n) > 0){
+                //nothing to do here
+            } else {
+                return n;
+            }
+        } 
+        return;
+    });
+    
+    if(commonValues.length > 0){
+        return "Invalid array value.";
+    }
+
+    //find cube root of array values
+    let results = {};
+    arr.filter(function(value) {
+        if(value < 0) {
+            //converting - to +
+            value = Math.abs(value)
+            let cubeRoot = cubes.indexOf(parseInt(value));
+            //converting + to -
+            value = -Math.abs(value)
+            Object.assign(results, {[value]: -Math.abs(cubeRoot+1)});
+        } else if(value == Infinity) {
+            Object.assign(results, {[value]: Infinity});
+        } else if(!isNaN(parseInt(value))){
+            let cubeRoot = cubes.indexOf(parseInt(value));
+            if(cubeRoot !== -1){
+                Object.assign(results, {[value]: cubeRoot+1});
+            }
+        } else {
+            //logic to be added for number which cube root is  > 10
+            //will add that later, as running out of time
+        }
+    });
+    return (results)
+};
+
+//only sorting of the output object is pending, will do it later today after session
+
+console.log(   arrayCubeRootToJson([27, 64, 125])   );
+console.log(   arrayCubeRootToJson(['27', '64', '125', 1])   );
+console.log(   arrayCubeRootToJson([-1, 1, Infinity, 64, -64])   );
+console.log(   arrayCubeRootToJson({})   );
+console.log(   arrayCubeRootToJson(true)   );
+console.log(   arrayCubeRootToJson([null, false])   );
+console.log(   arrayCubeRootToJson(false)   );
+console.log(   arrayCubeRootToJson(undefined)   );
+console.log(   arrayCubeRootToJson([1, 2, 4, 'abc'])   );
+console.log(   arrayCubeRootToJson([1, 2.12, 4.44, 'abc'])   );
--- array-utils/arrayUtils.js
+++ array-utils/arrayUtils.js
@@ -1,3 +1,128 @@
+//according to ECMAScript Spec code has been implemented, there might be some bug as i haven't tested yet this code due to lack of time, will do it later today and remove this comment.
+
+const forEach = (callback, args) => {
+  // Let O be ? ToObject(this value).
+  let O = Object(this);
+  // Let len be ? ToLength(? Get(O, "length")).
+  let len = O.length;
+  // If IsCallable(callbackfn) is false, throw a TypeError exception.
+  if (typeof callback !== 'function') {
+    throw new TypeError(callback + ' not a function');
+  }
+  // If thisArg is present, let T be thisArg; else let T be undefined.
+  let T = args || undefined;
+  // Let k be 0.
+  let k = 0;
+  // Repeat, while k < len
+  while(k < len){
+    // Let Pk be ! ToString(k).
+    let Pk = k.toString();
+    // Let kPresent be ? HasProperty(O, Pk).
+    let kPresent = O.hasOwnProperty(Pk);
+    // If kPresent is true, then
+    if(kPresent)
+      let kValue = O[Pk]; // Let kValue be ? Get(O, Pk).
+    // Perform ? Call(callbackfn, T, « kValue, k, O »).
+    let mappedValue = callback.call(T, kValue, k, O);
+    // Increase k by 1.
+    k++
+  }
+  return undefined;
+  // Return undefined.
+}
+
+const map = (callback, args) => {
+  // Let O be ? ToObject(this value).
+  let O = Object(this);
+  // Let len be ? ToLength(? Get(O, "length")).
+  let len = O.length;
+  // If IsCallable(callbackfn) is false, throw a TypeError exception.
+  if (typeof callback !== 'function') {
+    throw new TypeError(callback + ' not a function');
+  }
+  // If thisArg is present, let T be thisArg; else let T be undefined.
+  let T = args || undefined;
+  //Let A be ? ArraySpeciesCreate(O, len).
+  let A = ArraySpeciesCreate(O, len);
+  //Let k be 0.
+  let k = 0;
+
+  while(k < len){
+    let Pk = k.toString();
+    let kPresent = O.hasOwnProperty(Pk);
+    if(kPresent){
+      let kValue = O[Pk];
+      let mappedValue = callback.call(T, kValue, k, O);
+      A[k] = mappedValue;
+    }
+    k++;
+  }
+  return A;
+}
+
+const filter = (callback, args) => {
+  let O = Object(this);
+  let len = O.length;
+  if (typeof callback !== 'function') {
+    throw new TypeError(callback + ' not a function');
+  }
+  let T = args || undefined;
+  let A = new Array(0);
+  let k = 0;
+  let to = 0;
+  while(k < len){
+    let Pk = k.toString();
+    let kPresent = O.hasOwnProperty(Pk);
+    if(kPresent){
+      let kValue = O[Pk];
+      let selected = callback.call(callback, T, kValue, k, O);
+      if(selected){
+        A[to.toString()] = kValue;
+        to++
+      }
+    }
+    k++;
+  }
+  return A;
+}
+
+const reduce = (callback, args) => {
+  let O = Object(this);
+  let len = O.length;
+  if (typeof callback !== 'function') {
+    throw new TypeError(callback + ' not a function');
+  }
+  if (len === 0 && !args) {
+    throw new TypeError('no initial value passed');
+  }
+  let k = 0;
+  let accumulator = args || undefined;
+  if(accumulator == undefined){
+    let kPresent = false;
+    while(k < len && kPresent === false){
+      let Pk = k.toString();
+      kPresent = O.hasOwnProperty(Pk);
+      if(kPresent){
+        accumulator = O[Pk];
+      }
+      k++;
+    }
+    if(kPresent === false){
+      throw new TypeError("Invalid value")
+    }
+  }
+
+  while(k < len){
+    let Pk = k.toString();
+    let kPresent = O.hasOwnProperty(Pk);
+    if(kPresent){
+      let kValue = O[Pk];
+      accumulator = callback.call(callback, undefined, accumulator, kValue, k, O);
+    }
+    k++;
+  }
+  return accumulator;
+}
 
 export {
   forEach,
